1dd045808ec46c1e7e65301a205e1c61dc47ee84
Elmar Brauch <elmar.brauch@telekom.de>
2021-10-30 21:54:01 +0200

    Typo fixed

--- src/main/kotlin/de/bsi/basics/kotlin/NullSafety.kt
+++ src/main/kotlin/de/bsi/basics/kotlin/NullSafety.kt
@@ -1,7 +1,7 @@
 package de.bsi.basics.kotlin
 
 fun main() {
-    val test = NullSafety("Compile does not allow null here!", "String? can be null")
+    val test = NullSafety("Compiler does not allow null here!", "String? can be null")
     test.nullableText = null
 
     // The following lines produce compile errors due to Kotlin's null safety
